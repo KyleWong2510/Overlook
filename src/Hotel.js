1195d78b9955cda9e01513abc6ea5161f6932e0b
Kyle Wong <kylewong2510@gmail.com>
2020-04-18 16:15:16 -0600

    Pass some tests

--- src/Hotel.js
+++ src/Hotel.js
@@ -9,7 +9,32 @@ class Hotel {
     this.upcomingBookings = [];
   }
 
+  pushAllRooms(rooms) {
+    rooms.forEach(room => this.allRooms.push(room))
+  };
+
+  pushAllBookings(bookings) {
+    bookings.forEach(booking => this.allBookings.push(booking))
+  };
   
+  filterRoomsBooked(date) {
+    this.allBookings.forEach(booking => {
+      return this.allRooms.forEach(room => {
+        if(booking.date === date && booking.roomNumber === room.number) {
+          this.roomsBooked.push(room)
+        } 
+      })
+    })
+  }
+
+  filterRoomsAvailable(date) {
+    this.allBookings.filter(booking => {
+      booking.date !== date
+    })
+    this.allRooms.forEach(room => {
+      
+    })
+  }
 }
 
 export default Hotel
